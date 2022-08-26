af83edaba3bffc8fdc14bb835c00cafd05917503
Pablo Torres <pablotorreeifel@gmail.com>
2022-08-26 16:10:38 -0300

    Order enum

--- src/main/java/com/torres/springBoot/config/TestConfig.java
+++ src/main/java/com/torres/springBoot/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.torres.springBoot.entities.Order;
 import com.torres.springBoot.entities.User;
+import com.torres.springBoot.entities.enums.OrderStatus;
 import com.torres.springBoot.repositories.OrderRepository;
 import com.torres.springBoot.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Paulo", "paulo@gmail.com", "99999999999", "123456");
 		User u2 = new User(null, "João", "joao@gmail.com", "99999999999", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2022-08-20T12:41:11Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2022-08-20T12:41:11Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2022-08-20T10:43:22Z"), u2);
+		Order o1 = new Order(null, Instant.parse("2022-08-20T12:41:11Z"), OrderStatus.WAIT_PAYMENT, u1);
+		Order o2 = new Order(null, Instant.parse("2022-08-20T12:41:11Z"), OrderStatus.PAID, u2);
+		Order o3 = new Order(null, Instant.parse("2022-08-20T10:43:22Z"), OrderStatus.CANCELED, u2);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
