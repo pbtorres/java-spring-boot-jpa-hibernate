9b5f914a9f3cfbd00ec854ae6f746f19f6f83b2c
Pablo Torres <pablotorreeifel@gmail.com>
2022-08-28 15:25:45 -0300

    OrderItem, many-to-many association with extras attributes PART 1

--- src/main/java/com/torres/springBoot/entities/Product.java
+++ src/main/java/com/torres/springBoot/entities/Product.java
@@ -31,7 +31,7 @@ public class Product implements Serializable {
 	@JoinTable(name = "products_categories",
 			joinColumns = @JoinColumn(name = "product_id"),
 			inverseJoinColumns = @JoinColumn(name = "category_id"))
-	private Set<Category> categories = new HashSet<>() ;
+	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {
 		
